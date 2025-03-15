3967e3ec7f9706b4431936bc0bf10e82251df29b
ShaYri12 <sharjeelb60@gmail.com>
2025-03-16 03:32:18 +0500

    removed post button

--- src/pages/ShipsPage.jsx
+++ src/pages/ShipsPage.jsx
@@ -300,12 +300,12 @@ const ShipsPage = () => {
               selectedShip={selectedShip}
               onShipChange={handleShipChange}
             />
-            <button
+            {/* <button
               onClick={handleAddShip}
               className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
             >
               Add New Ship
-            </button>
+            </button> */}
           </div>
         </div>
 
